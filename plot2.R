2a54c379012a2e78c66a35395cf5bd896d11f33a
Fabrizio <fabryqa@hotmail.es>
2020-08-16 15:46:15 -0500

    resizing plot 2

--- plot2.R
+++ plot2.R
@@ -20,7 +20,7 @@ data$DateTime <- as.POSIXct(DateTime)
 
 
 #Plot 2
-with(data, plot(DateTime, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = ""))
+plot(data$DateTime, data$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
 dev.copy(png,"plot2.png", width=480, height=480)
 dev.off()
 
